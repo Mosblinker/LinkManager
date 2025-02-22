5abb93d02ede87ee79bfbd08a63f4e1b3451004d
Mosblinker <msteier2.0@gmail.com>
2025-02-22 11:03:51 -0600

    Added a map view for list data stored in config
    I've added an abstract map view to use to view the data in the preference node for list types and list data. It's currently not used by anything yet.

--- src/manager/LinkManagerConfig.java
+++ src/manager/LinkManagerConfig.java
@@ -1739,4 +1739,180 @@ public class LinkManagerConfig {
             clearDropboxToken();
         }
     }
+    /**
+     * 
+     * @param <V> 
+     */
+    private abstract class ListDataMap<V> extends AbstractMap<Integer,V>{
+        /**
+         * This is an array containing the entries in this map. This is 
+         * initially null and is initialized the first time it is used.
+         */
+        private Set<Entry<Integer, V>> entries = null;
+        /**
+         * This returns the value to which the specified key is mapped to, or 
+         * null if this map contains no mapping for the key. This is called by 
+         * the {@code get} method to get the value to return.
+         * @param key The key whose associated value is to be returned. (Will 
+         * not be null)
+         * @return The value to which the specified key is mapped to, or null if 
+         * this map contains no mapping for the key.
+         */
+        protected abstract V getValue(Integer key);
+        @Override
+        public V get(Object key){
+                // Require the key to not be null
+            Objects.requireNonNull(key, "Key cannot be null");
+                // If the key is an integer
+            if (key instanceof Integer)
+                    // Get the value
+                return getValue((Integer)key);
+            else
+                throw new ClassCastException();
+        }
+        @Override
+        public boolean containsKey(Object key){
+                // If the given key is an integer
+            if (key instanceof Integer)
+                return getValue((Integer)key) != null;
+            return false;
+        }
+        /**
+         * Associates the given value with the given key in this map, or removes 
+         * the given key if the given value is null. This is called by the 
+         * {@code put} method to put the values and {@code remove} method to 
+         * remove the key.
+         * @param key The key with which the given value is to be associated 
+         * with. (Will not be null)
+         * @param value The value to be associated with the given key, or null 
+         * if the key is to be removed.
+         * @throws ClassCastException If the class of the given key or value 
+         * prevents it from being stored in this map.
+         * @see IllegalArgumentException If some property of the given key or 
+         * value prevents it from being stored in this map.
+         */
+        protected abstract void putValue(Integer key, V value);
+        @Override
+        public V put(Integer key, V value){
+                // Require the key to not be null
+            Objects.requireNonNull(key, "Key cannot be null");
+                // Require the value to not be null
+            Objects.requireNonNull(value, "Value cannot be null");
+                // Get the old value for the key
+            V old = getValue(key);
+                // Put the value into the map
+            putValue(key,value);
+            return old;
+        }
+        @Override
+        public V remove(Object key){
+                // Require the key to not be null
+            Objects.requireNonNull(key, "Key cannot be null");
+                // If the key is an integer
+            if (key instanceof Integer){
+                    // Get the key as an integer
+                Integer keyInt = (Integer) key;
+                    // Get the old value for the key
+                V old = getValue(keyInt);
+                    // Put null for the value into the map
+                putValue(keyInt,null);
+                return old;
+            } else
+                throw new ClassCastException();
+        }
+        /**
+         * 
+         * @return 
+         */
+        protected abstract String getPrefixForNodes();
+        /**
+         * This returns a set containing the keys for this map.
+         * @return The set containing the keys for this map.
+         */
+        protected Set<Integer> getKeys(){
+                // This will get the keys in this map
+            Set<Integer> keys = new TreeSet<>();
+            try{    // Get the names of the child nodes in the local preference 
+                String[] childNodes = getPreferences().childrenNames(); // node
+                    // Go through the names of the child nodes
+                for (String child : childNodes){
+                        // If the child's name is not null and starts with the 
+                        // prefix
+                    if (child != null && child.startsWith(getPrefixForNodes())){
+                        try{    // Parse the number at the end
+                            int value = Integer.parseInt(child.substring(
+                                    getPrefixForNodes().length()));
+                                // If this map contains a non-null value for 
+                            if (containsKey(value))     // that key
+                                keys.add(value);
+                        } catch (NumberFormatException ex) {}
+                    }
+                }
+            } catch (BackingStoreException ex) {}
+            return keys;
+        }
+        @Override
+        public Set<Entry<Integer, V>> entrySet() {
+                // If the entry set is null
+            if (entries == null){
+                    // Create a new AbstractSet to go through the entries
+                entries = new AbstractSet<>(){
+                    @Override
+                    public Iterator<Entry<Integer, V>> iterator() {
+                        return new Iterator<>(){
+                            /**
+                             * Get an iterator to go over the keys.
+                             */
+                            Iterator<Integer> itr = getKeys().iterator();
+                            /**
+                             * This is the most recent key with an entry 
+                             * returned for.
+                             */
+                            Integer current = null;
+                            @Override
+                            public boolean hasNext() {
+                                return itr.hasNext();
+                            }
+                            @Override
+                            public Entry<Integer, V> next() {
+                                    // Get the next value in the key iterator
+                                current = itr.next();
+                                return new Entry<>(){
+                                    /**
+                                     * The key for this entry
+                                     */
+                                    Integer key = current;
+                                    @Override
+                                    public Integer getKey() {
+                                        return key;
+                                    }
+                                    @Override
+                                    public V getValue() {
+                                        return ListDataMap.this.getValue(key);
+                                    }
+                                    @Override
+                                    public V setValue(V value) {
+                                        return put(key,value);
+                                    }
+                                };
+                            }
+                            @Override
+                            public void remove() {
+                                    // Remove the value, mostly to throw any 
+                                    // exceptions
+                                itr.remove();
+                                    // Actually remove the value from the map
+                                putValue(current,null);
+                            }
+                        };
+                    }
+                    @Override
+                    public int size() {
+                        return getKeys().size();
+                    }
+                };
+            }
+            return entries;
+        }
+    }
 }
