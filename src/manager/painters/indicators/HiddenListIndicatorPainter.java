9945cc777bc2eb43cfc656c8ac2fec9407970ecc
Mosblinker <msteier2.0@gmail.com>
2025-02-26 01:02:41 -0600

    Added unimplemented list indicator painters
    I've added painters for drawing the read only list indicator and the hidden list indicator, however these have not been implemented yet.

---
+++ src/manager/painters/indicators/HiddenListIndicatorPainter.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package manager.painters.indicators;
+
+import java.awt.*;
+
+/**
+ * This is the list indicator to indicate a list is hidden.
+ * @author Mosblinker
+ */
+public class HiddenListIndicatorPainter extends AbstractListIndicatorPainter{
+
+    @Override
+    protected void paintIndicator(Graphics2D g, Component c, int width, int height) {
+        
+    }
+}
---
+++ src/manager/painters/indicators/ReadOnlyListIndicatorPainter.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package manager.painters.indicators;
+
+import java.awt.*;
+
+/**
+ * This is the list indicator to indicate a list is read only.
+ * @author Mosblinker
+ */
+public class ReadOnlyListIndicatorPainter extends AbstractListIndicatorPainter{
+
+    @Override
+    protected void paintIndicator(Graphics2D g, Component c, int width, int height) {
+        
+    }
+}
