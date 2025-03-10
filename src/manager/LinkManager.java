a9b605b199d2f88622ce0916953fd5f9df738903
Mosblinker <msteier2.0@gmail.com>
2025-03-10 15:11:41 -0500

    Using remove collection to remove list nodes
    I've made it so that the list preference nodes for the removed lists are now removed using the method that takes in a collection.

--- src/manager/LinkManager.java
+++ src/manager/LinkManager.java
@@ -9036,13 +9036,9 @@ public class LinkManager extends JFrame implements DisableGUIInput,DebugCapable{
             conn.getShownListIDs().removeAll(shownListsTabsPanel.getRemovedListIDs());
                 // Remove any lists that have been removed
             conn.getListNameMap().keySet().removeAll(allListsTabsPanel.getRemovedListIDs());
-                // Go through the removed list IDs
-            for (Integer listID : allListsTabsPanel.getRemovedListIDs()){
-                    // If the current listID is not null
-                if (listID != null)
-                        // Remove the list's preference node
-                    config.removeListPreferences(listID);
-            }   // Clear the sets of removed ListIDs
+                // Remove the preference nodes for the removed lists
+            config.removeListPreferences(allListsTabsPanel.getRemovedListIDs());
+                // Clear the sets of removed ListIDs
             allListsTabsPanel.clearRemovedListIDs();
             shownListsTabsPanel.clearRemovedListIDs();
             conn.commit();       // Commit the changes to the database
