79649ceee4c6fc1ad60fc01de20bd7d0e4c2b62f
Mosblinker <msteier2.0@gmail.com>
2025-03-22 15:58:35 -0500

    Created a new CipherUtils class
    I've re-added the CipherUtils class which currently only has a SecureRandom object.

---
+++ src/manager/security/CipherUtils.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package manager.security;
+
+import java.security.*;
+import java.util.Objects;
+
+/**
+ *
+ * @author Mosblinker
+ */
+public class CipherUtils {
+    /**
+     * This is the SecureRandom used to generate random numbers for the cipher.
+     */
+    private SecureRandom rand;
+    /**
+     * 
+     * @param rand 
+     */
+    public CipherUtils(SecureRandom rand){
+        this.rand = Objects.requireNonNull(rand);
+    }
+    /**
+     * 
+     * @throws java.security.NoSuchAlgorithmException
+     */
+    public CipherUtils() throws NoSuchAlgorithmException{
+        this(SecureRandom.getInstanceStrong());
+    }
+    /**
+     * 
+     * @param utils 
+     */
+    public CipherUtils(CipherUtils utils){
+        this.rand = utils.rand;
+    }
+    /**
+     * 
+     * @return 
+     */
+    public SecureRandom getSecureRandom(){
+        return rand;
+    }
+    /**
+     * 
+     * @param rand 
+     */
+    public void setSecureRandom(SecureRandom rand){
+        this.rand = rand;
+    }
+}
