f90c19eeb786fc2a9c858bdf8e98e4805756615c
Mosblinker <msteier2.0@gmail.com>
2025-08-09 18:49:02 -0500

    Incremented program version
    I've incremented the program version to 0.12.0

--- src/manager/LinkManager.java
+++ src/manager/LinkManager.java
@@ -72,7 +72,7 @@ public class LinkManager extends JFrame implements DisableGUIInput,DebugCapable{
     /**
      * This is the version of the program.
      */
-    public static final String PROGRAM_VERSION = "0.11.6";
+    public static final String PROGRAM_VERSION = "0.12.0";
     /**
      * This is the internal name for the program.
      */
