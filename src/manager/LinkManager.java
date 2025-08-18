02ce26d93108f131c4d24b17015e0783c29f41d8
Mosblinker <msteier2.0@gmail.com>
2025-08-18 16:24:55 -0500

    Incremented program version
    I've incremented the program version to 0.14.2-alpha.

--- src/manager/LinkManager.java
+++ src/manager/LinkManager.java
@@ -72,7 +72,7 @@ public class LinkManager extends JFrame implements DisableGUIInput,DebugCapable{
     /**
      * This is the version of the program.
      */
-    public static final String PROGRAM_VERSION = "0.14.1";
+    public static final String PROGRAM_VERSION = "0.14.2-alpha";
     /**
      * This is the internal name for the program.
      */
