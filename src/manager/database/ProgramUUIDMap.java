57c884de390c3c70d317b0ec36d59fe939ed53d2
Mosblinker <msteier2.0@gmail.com>
2025-11-06 14:21:43 -0600

    Added map to get IDs for program IDs
    I've added a map that gets the database's IDs for a program.

---
+++ src/manager/database/ProgramUUIDMap.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package manager.database;
+
+import java.util.UUID;
+import sql.util.SQLMap;
+
+/**
+ * This is a map view that maps the program UUIDs associated with a given user 
+ * UUID to the row IDs the rows.
+ * @author Mosblinker
+ */
+public interface ProgramUUIDMap extends SQLMap<UUID,Integer>{
+    /**
+     * 
+     * @return 
+     */
+    public UUID getUserID();
+}
