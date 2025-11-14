27e0afd175ca031f892fe7e2d2015c31c12e8fe6
Mosblinker <msteier2.0@gmail.com>
2025-11-14 13:05:14 -0600

    Reworked formatting

--- src/manager/util/EditListAction.java
+++ src/manager/util/EditListAction.java
@@ -27,89 +27,81 @@ import javax.swing.border.*;
  * 
  * @author Rob Camick
  */
-public class EditListAction extends AbstractAction
-{
-	private JList list;
+public class EditListAction extends AbstractAction {
+    private JList list;
 
-	private JPopupMenu editPopup;
-	private JTextField editTextField;
-	private Class<?> modelClass;
+    private JPopupMenu editPopup;
+    private JTextField editTextField;
+    private Class<?> modelClass;
 
-	public EditListAction()
-	{
-		setModelClass(DefaultListModel.class);
-	}
+    public EditListAction() {
+        setModelClass(DefaultListModel.class);
+    }
 
-	protected void setModelClass(Class modelClass)
-	{
-		this.modelClass = modelClass;
-	}
+    protected void setModelClass(Class modelClass) {
+        this.modelClass = modelClass;
+    }
 
-	protected void applyValueToModel(String value, ListModel model, int row)
-	{
-		DefaultListModel dlm = (DefaultListModel)model;
-		dlm.set(row, value);
-	}
+    protected void applyValueToModel(String value, ListModel model, int row){
+        DefaultListModel dlm = (DefaultListModel)model;
+        dlm.set(row, value);
+    }
 
-	/*
-	 *	Display the popup editor when requested
-	 */
-	public void actionPerformed(ActionEvent e)
-	{
-		list = (JList)e.getSource();
-		ListModel model = list.getModel();
+    /*
+     *	Display the popup editor when requested
+     */
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        list = (JList)e.getSource();
+        ListModel model = list.getModel();
 
-		if (! modelClass.isAssignableFrom(model.getClass())) return;
+        if (! modelClass.isAssignableFrom(model.getClass())) 
+            return;
 
-		//  Do a lazy creation of the popup editor
+            //  Do a lazy creation of the popup editor
 
-		if (editPopup == null)
-    		createEditPopup();
+        if (editPopup == null)
+            createEditPopup();
 
-		//  Position the popup editor over top of the selected row
+            //  Position the popup editor over top of the selected row
 
-		int row = list.getSelectedIndex();
-		Rectangle r = list.getCellBounds(row, row);
+        int row = list.getSelectedIndex();
+        Rectangle r = list.getCellBounds(row, row);
 
-		editPopup.setPreferredSize(new Dimension(r.width, r.height));
-		editPopup.show(list, r.x, r.y);
+        editPopup.setPreferredSize(new Dimension(r.width, r.height));
+        editPopup.show(list, r.x, r.y);
 
-		//  Prepare the text field for editing
+            //  Prepare the text field for editing
 
-		editTextField.setText( list.getSelectedValue().toString() );
-		editTextField.selectAll();
-		editTextField.requestFocusInWindow();
-	}
+        editTextField.setText( list.getSelectedValue().toString() );
+        editTextField.selectAll();
+        editTextField.requestFocusInWindow();
+    }
 
-	/*
-	 *  Create the popup editor
-	 */
-	private void createEditPopup()
-	{
-		//  Use a text field as the editor
+    /*
+     *  Create the popup editor
+     */
+    private void createEditPopup(){
+            //  Use a text field as the editor
 
-		editTextField = new JTextField();
-		Border border = UIManager.getBorder("List.focusCellHighlightBorder");
-		editTextField.setBorder( border );
+        editTextField = new JTextField();
+        Border border = UIManager.getBorder("List.focusCellHighlightBorder");
+        editTextField.setBorder( border );
 
-		//  Add an Action to the text field to save the new value to the model
+            //  Add an Action to the text field to save the new value to the model
 
-		editTextField.addActionListener(new ActionListener()
-		{
-			public void actionPerformed(ActionEvent e)
-			{
-				String value = editTextField.getText();
-				ListModel model = list.getModel();
-				int row = list.getSelectedIndex();
-				applyValueToModel(value, model, row);
-				editPopup.setVisible(false);
-			}
-		});
+        editTextField.addActionListener((ActionEvent e) -> {
+            String value = editTextField.getText();
+            ListModel model = list.getModel();
+            int row = list.getSelectedIndex();
+            applyValueToModel(value, model, row);
+            editPopup.setVisible(false);
+        });
 
-		//  Add the editor to the popup
+            //  Add the editor to the popup
 
-	    editPopup = new JPopupMenu();
-		editPopup.setBorder( new EmptyBorder(0, 0, 0, 0) );
-    	editPopup.add(editTextField);
-	}
+        editPopup = new JPopupMenu();
+        editPopup.setBorder( new EmptyBorder(0, 0, 0, 0) );
+        editPopup.add(editTextField);
+    }
 }
