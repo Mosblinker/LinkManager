bd87631cbdde01b5c76b2406fac434003ec17bb0
Mosblinker <msteier2.0@gmail.com>
2025-11-13 18:22:22 -0600

    Implemented Metadata name list renderer

--- src/manager/dropbox/MetadataNameListCellRenderer.java
+++ src/manager/dropbox/MetadataNameListCellRenderer.java
@@ -4,10 +4,32 @@
  */
 package manager.dropbox;
 
+import com.dropbox.core.v2.files.Metadata;
+import java.awt.Component;
+import javax.swing.DefaultListCellRenderer;
+import javax.swing.JList;
+
 /**
  *
  * @author Mosblinker
  */
-public class MetadataNameListCellRenderer {
-    
+public class MetadataNameListCellRenderer extends DefaultListCellRenderer{
+    /**
+     * 
+     * @param list
+     * @param value
+     * @param index
+     * @param isSelected
+     * @param cellHasFocus
+     * @return 
+     */
+    @Override
+    public Component getListCellRendererComponent(JList list,
+            Object value,int index,boolean isSelected,boolean cellHasFocus){
+        if (value instanceof Metadata){
+            value = ((Metadata)value).getName();
+        }
+        return super.getListCellRendererComponent(list, value, index, 
+                isSelected, cellHasFocus);
+    }
 }
