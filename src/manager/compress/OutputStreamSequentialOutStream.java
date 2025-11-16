db5a6e00a6eac8eef4f2738c08e87155fa48511c
Mosblinker <msteier2.0@gmail.com>
2025-11-15 18:01:16 -0600

    Writing progress is now being observed
    I've added a progress observer to the output stream wrapper class.

--- src/manager/compress/OutputStreamSequentialOutStream.java
+++ src/manager/compress/OutputStreamSequentialOutStream.java
@@ -5,6 +5,7 @@
 package manager.compress;
 
 import java.io.*;
+import manager.ProgressObserver;
 import net.sf.sevenzipjbinding.*;
 
 /**
@@ -19,12 +20,43 @@ public class OutputStreamSequentialOutStream implements ISequentialOutStream,
     private final OutputStream out;
     /**
      * 
-     * @param out 
      */
-    public OutputStreamSequentialOutStream(OutputStream out){
+    private ProgressObserver observer;
+    /**
+     * 
+     */
+    private long size = 0L;
+    /**
+     * 
+     * @param out 
+     * @param l 
+     */
+    public OutputStreamSequentialOutStream(OutputStream out, ProgressObserver l){
         if (out == null)
             throw new NullPointerException();
         this.out = out;
+        observer = l;
+    }
+    /**
+     * 
+     * @param out 
+     */
+    public OutputStreamSequentialOutStream(OutputStream out){
+        this(out,null);
+    }
+    /**
+     * 
+     * @return 
+     */
+    public ProgressObserver getProgressObserver(){
+        return observer;
+    }
+    /**
+     * 
+     * @param l 
+     */
+    public void setProgressObserver(ProgressObserver l){
+        observer = l;
     }
     @Override
     public int write(byte[] data) throws SevenZipException {
@@ -33,6 +65,9 @@ public class OutputStreamSequentialOutStream implements ISequentialOutStream,
         } catch (IOException ex){
             throw new SevenZipException(ex);
         }
+        size += data.length;
+        if (observer != null)
+            observer.setValueLong(size);
         return data.length;
     }
     @Override
